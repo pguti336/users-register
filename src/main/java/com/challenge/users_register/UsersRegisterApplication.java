373f33339bdfc8d7a2397519c49f72fe4d27a410
pguti336 <patogutierrez.336@gmail.com>
2024-07-12 14:18:26 -0400

    :tada: Init Spring boot project

---
+++ src/main/java/com/challenge/users_register/UsersRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.users_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRegisterApplication.class, args);
+	}
+
+}
